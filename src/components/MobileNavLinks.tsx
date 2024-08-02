56860449cf2c77b075dbf86f661be2281ecf0d64
kunaltripathi7 <ikunaltripathi@gmail.com>
2024-08-03 00:50:28 +0530

    added view order feature

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -10,6 +10,18 @@ const MobileNavLinks = () => {
       >
         User Profile
       </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Manage Restaurant
+      </Link>
+      <Link
+        to="/order-status"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        My Orders
+      </Link>
       <Button className="flex items-center px-3 font-bold hover:bg-gray-500">
         Log Out
       </Button>
