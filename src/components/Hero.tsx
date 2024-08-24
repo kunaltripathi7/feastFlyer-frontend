2305d330e702cd750a6f7b072c785ff7f4c75776
kunaltripathi7 <ikunaltripathi@gmail.com>
2024-08-25 01:48:23 +0530

    added hero-loader

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import hero from "../assets/hero.png";
+import { FadeLoader } from "react-spinners";
+
 function Hero() {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div>
+    <div className="relative">
+      {loading && (
+        <div className="flex justify-center items-center h-96">
+          <FadeLoader color="#000000" />
+        </div>
+      )}
       <img
         src={hero}
-        className="w-full max-h-[600px] object-cover"
+        className={`w-full max-h-[600px] object-cover ${
+          loading ? "hidden" : "block"
+        }`}
         alt="Cover Image"
+        onLoad={() => setLoading(false)}
       />
     </div>
   );
